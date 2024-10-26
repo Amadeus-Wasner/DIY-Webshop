6a15217c090aece71cebb124d55f459560a269d8
Amadeus Wasner <amadeus.wasner@protonmail.com>
2024-10-26 15:15:06 +0200

    Initial commit

---
+++ public/login.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Willkommen im DIY-Webshop</title>
+</head>
+<body>
+    <h1>DIY-Kits Webshop - Willkommen!</h1>
+    <p>Hier wird bald dein DIY-Kit Webshop entstehen.</p>
+</body>
+</html>
